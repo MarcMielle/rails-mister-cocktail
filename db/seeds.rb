be8544c91cccce9400a1f034d9605b083c999508
MarcMielle <marcmielle96@gmail.com>
2020-08-13 17:20:08 +0200

    Backend MVC + R rails app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all if Rails.env.development?
+
 Ingredient.create(name: "Lemon")
 Ingredient.create(name: "Ice")
 Ingredient.create(name: "Mint Leaves")
